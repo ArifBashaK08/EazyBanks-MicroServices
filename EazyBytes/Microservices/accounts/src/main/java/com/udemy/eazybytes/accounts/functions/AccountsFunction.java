95a32630cd29cedb258ab335e716505990c23aff
Arif Basha K. <arifkolimi08@gmail.com>
2025-05-09 10:40:27 +0530

    Implemented Async services with RabbitMQ - fixed error

--- EazyBytes/Microservices/accounts/src/main/java/com/udemy/eazybytes/accounts/functions/AccountsFunction.java
+++ EazyBytes/Microservices/accounts/src/main/java/com/udemy/eazybytes/accounts/functions/AccountsFunction.java
@@ -3,12 +3,16 @@ package com.udemy.eazybytes.accounts.functions;
 import com.udemy.eazybytes.accounts.services.IAccountsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
+@Configuration
 public class AccountsFunction {
     private static final Logger logger = LoggerFactory.getLogger(AccountsFunction.class);
 
+    @Bean
     public Consumer<Long> updateCommunication(IAccountsService iAccountsService){
         return accountNumber -> {
             logger.info("Updating communication status for the account number: {}", accountNumber.toString());
